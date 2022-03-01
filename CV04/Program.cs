e59ccb77e93ed9981121df1b5c3bf91ececf3cb0
Cristovao1999 <cristovao1999cristovao@hotmail.com>
2022-03-01 11:34:59 +0100

    CV04

---
+++ CV04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV04
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+                                   + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                                   + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                                   + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                                   + "posledni veta!\n";
+            StringStatistics textem = new StringStatistics();
+            Console.WriteLine(testovaciText + "\n");
+            Console.WriteLine($"Pocet slov: {textem.pocetSlov(testovaciText)}");
+            Console.WriteLine($"Pocet radku: {textem.pocetRadku(testovaciText)}");
+            Console.WriteLine($"Pocet vet: {textem.pocetVet(testovaciText)}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CV04/StringStatistics.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV04
+{
+    internal class StringStatistics
+    {
+        protected int PocetSlov = 0, PocetRadku = 0, PocetVet = 0;
+        public int pocetSlov(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ',' || text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == ' ')
+                {
+                    PocetSlov = PocetSlov + 1;
+                }
+            }
+            return PocetSlov;
+        }
+        //hotovo
+        public int pocetRadku(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '\n')
+                {
+                    PocetRadku = PocetRadku + 1;
+                }
+            }
+            return PocetRadku;
+        }
+        //hotovo
+        public int pocetVet(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+                {
+                    PocetVet = PocetVet + 1;
+                }
+            }
+            return PocetVet;
+        }
+    }
+}
