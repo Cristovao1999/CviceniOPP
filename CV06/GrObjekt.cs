1e34eaccbbd3af7f4e9e9a3fe639aa28ceb2b411
Cristovao1999 <73932019+Cristovao1999@users.noreply.github.com>
2022-03-15 07:32:25 +0100

    Add files via upload

---
+++ CV06/GrObjekt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV06
+{
+    abstract class GrObjekt
+    {
+       
+    }
+    interface IGrObjekt
+    {
+        void kresli();
+    }
+
+}
